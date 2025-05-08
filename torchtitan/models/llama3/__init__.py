620c9f9d12591431a35f8d0fb19c6986b650116c
lc <lcvcl@Outlook.com>
2025-05-08 20:07:26 +0800

    Update __init__.py
    add 1.5b llama

--- torchtitan/models/llama3/__init__.py
+++ torchtitan/models/llama3/__init__.py
@@ -38,6 +38,16 @@ llama3_configs = {
         use_flex_attn=True,
         attn_mask_type="block_causal",
     ),
+    "1.5B": TransformerModelArgs(
+        dim=2048,
+        n_layers=24,
+        n_heads=16,
+        n_kv_heads=8,
+        ffn_dim_multiplier=1.3,
+        multiple_of=512,
+        rope_theta=500000,
+        use_flex_attn=True,
+    ),
     "8B": TransformerModelArgs(
         dim=4096,
         n_layers=32,
